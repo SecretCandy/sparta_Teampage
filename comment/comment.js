0108f491bcdfb52cd3b12e29cf4952007e6d8b0b
Yoon Gun-hee <yoongunhee92@gmail.com>
2024-04-16 15:46:33 +0900

    🔥 불필요한 파일 삭제

--- comment/comment.js
+++
@@ -1,9 +0,0 @@
-// 입력창에 문자를 입력하면 "댓글을 작성하였습니다", 아무 문자도 입력하지 않았으면 "댓글을 작성해주세요."라고 알림이 뜨도록 함
-function hey() {
-    var input = document.querySelector('.form-control').value;
-    if (input.trim() !== '') {
-        alert("댓글을 작성하였습니다.");
-    } else {
-        alert("댓글을 작성해주세요.");
-    }
-}
